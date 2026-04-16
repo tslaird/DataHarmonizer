658431890de236fca2f707c5e7702178ee1c611d
tslaird <tyler.laird@nist.gov>
2026-04-16 14:28:12 -0400

    fix: fixed path replacement script

--- script/fix-paths.sh
+++ script/fix-paths.sh
@@ -12,12 +12,12 @@ TARGET_DIR="$1"
 
 # Update template paths in any main.js files under docs
 # Update template paths in docs/scripts/main.js
-sed -i 's#templates/#/DataHarmonizer/templates/#g' "$TARGET_DIR/docs/scripts/main.js"
+sed -i 's#templates/#DataHarmonizer/templates/#g' "$TARGET_DIR/scripts/main.js"
 
-# Fix escaped regex pattern in any files (adjust if needed)
-find "$TARGET_DIR" -type f -exec sed -i 's#templates\/#/DataHarmonizer/templates\/\#g' {} +
+# Fix escaped regex pattern 
+sed -i 's#\\/templates\\/#\\/DataHarmonizer\\/templates\\/#g' "$TARGET_DIR/scripts/main.js"
 
 # Update HTML file references for schema and script paths
-find "$TARGET_DIR" -type f -name "*.html" -exec sed -i -e 's#href="index.html"#href="./DataHarmonizer/index.html"#g' -e 's#url=index.html#url=./DataHarmonizer/index.html#g' -e 's#dist-schemas/schemas.js#./dist-schemas/schemas.js#g' -e 's#scripts/main.js#./scripts/main.js#g' {} \;
+find "$TARGET_DIR" -type f -name "*.html" -exec sed -i -e 's#href="index.html"#href="./DataHarmonizer/index.html"#g' -e 's#dist-schemas/schemas.js#./dist-schemas/schemas.js#g' -e 's#scripts/main.js#./scripts/main.js#g' {} \;
 
 echo "Path fixes applied."
